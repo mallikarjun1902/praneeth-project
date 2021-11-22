110b2971ed5aec20f49cc7446e393a879d75ac96
Itikalamadhavi <itikalamadhavi007@gmail.com>
2021-11-22 22:20:02 +0530

    updating the landing page to 'master'

--- src/components/global components/landing/landing.js
+++ src/components/global components/landing/landing.js
@@ -2,9 +2,70 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div>
-            
-        </div>
+      <>        
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="suta.css" rel="stylesheet"/>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+  {/* <style>
+   img.center {
+    display: block;
+    margin: 0 auto;
+}
+</style> */}
+</head>
+<body>
+<div class="topnav">
+    <img src=""/>
+ <div class="container d-flex flex-row justify-content-end">
+  <input type="search" placeholder="Search.."/>
+  <img src="https://static.vecteezy.com/system/resources/previews/002/318/271/non_2x/user-profile-icon-free-vector.jpg" height="40" width="40"/>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSGsPHNDTRSXj2zKf1LQR3pvWEttfSMrN2yw&usqp=CAU" height="30" width="30"/>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5dIdyyUTo69D6o_niUIZNG_IZ1GoCgsExybjLwrb7WqduxwgV5p7_PCnVZk2L-P8v-KE&usqp=CAU" height="30" width="30"/>
+  </div>
+</div>
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <div class="container-fluid d-flex flex-row justify-content-center justify-content-space-between">
+    <ul class="navbar-nav">
+       
+      <li class="nav-item">
+        <a class="nav-link" href="#">SAREES</a>
+      </li>
+       
+      <li class="nav-item">
+        <a class="nav-link" href="#">BLOUSES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">MEN'S WEAR</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">COLLECTIONS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ACCESSORIES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">LOUNGE WEAR</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">DRESSES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME & LIVING</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">BEST SELLER</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</body>
+
+        </>
     );
 }
 
